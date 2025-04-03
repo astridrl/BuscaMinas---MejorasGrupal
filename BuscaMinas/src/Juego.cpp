f3b70d025bb82bc7997c5d4d2102ad36983c27c9
[Dulce18rt] <[dreyesp11@miumg.edu.gt]>
2025-04-03 09:48:22 -0600

    Tiempo_Dulce_Reyes

--- BuscaMinas/src/Juego.cpp
+++ BuscaMinas/src/Juego.cpp
@@ -1,210 +1,180 @@
 #include "Juego.h"
 #include <fstream>
 #include <unistd.h>
+#include <chrono> // Agregue Biblioteca para que permite medir intervalos de tiempo con precisión.(Dulce Reyes)
 
-//Inicializacion de victorias y perdidas
+// Inicializacion de victorias y perdidas
 int Juego::victorias = 0;
 int Juego::perdidas = 0;
 
 int Juego::aleatorio_en_rango(int minimo, int maximo)
-	{
-		return minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
-	}
+{
+    return minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
+}
 
-	int Juego::filaAleatoria()
-	{
-		return this->aleatorio_en_rango(0, this->tablero.getAlturaTablero() - 1);
-	}
+int Juego::filaAleatoria()
+{
+    return this->aleatorio_en_rango(0, this->tablero.getAlturaTablero() - 1);
+}
 
-	int Juego::columnaAleatoria()
-	{
-		return this->aleatorio_en_rango(0, this->tablero.getAnchoTablero() - 1);
-	}
+int Juego::columnaAleatoria()
+{
+    return this->aleatorio_en_rango(0, this->tablero.getAnchoTablero() - 1);
+}
 
-    //Se agregaron las reducciones (Britany)
-	Juego::Juego(Tablero tablero, int cantidadMinas, int vidas, int puntosPorMina) //inicializar las vidas en el constructor
-	{
-		this->tablero = tablero;
-		this->cantidadMinas = cantidadMinas;
-		this->vidas = vidas; //puntero para guardar las vidas
-		this->colocarMinasAleatoriamente();
-		//Valores para la reduccion y aumento de puntos
-		this->puntuacion = 0; //Puntuacion en 0
-		this->puntosPorMina = puntosPorMina;
-	}
+// Se agregaron las reducciones (Britany)
+Juego::Juego(Tablero tablero, int cantidadMinas, int vidas, int puntosPorMina)
+{
+    this->tablero = tablero;
+    this->cantidadMinas = cantidadMinas;
+    this->vidas = vidas; // puntero para guardar las vidas
+    this->colocarMinasAleatoriamente();
+    // Valores para la reduccion y aumento de puntos
+    this->puntuacion = 0; // Puntuacion en 0
+    this->puntosPorMina = puntosPorMina;
+}
 
-	void Juego::colocarMinasAleatoriamente()
-	{
-		int x, y, minasColocadas = 0;
+void Juego::colocarMinasAleatoriamente()
+{
+    int x, y, minasColocadas = 0;
+    while (minasColocadas < this->cantidadMinas)
+    {
+        x = this->columnaAleatoria();
+        y = this->filaAleatoria();
+        if (this->tablero.colocarMina(x, y))
+        {
+            minasColocadas++;
+        }
+    }
+}
 
-		while (minasColocadas < this->cantidadMinas)
-		{
-			x = this->columnaAleatoria();
-			y = this->filaAleatoria();
-			if (this->tablero.colocarMina(x, y))
-			{
-				minasColocadas++;
-			}
-		}
-	}
+int Juego::solicitarFilaUsuario()
+{
+    int fila = 0;
+    cout << "Ingresa la FILA en la que desea jugar: ";
+    cin >> fila;
+    return fila - 1;
+}
 
-	int Juego::solicitarFilaUsuario()
-	{
-		int fila = 0;
-		cout << "Ingresa la FILA en la que desea jugar: ";
-		cin >> fila;
-		return fila - 1;
-	}
+int Juego::solicitarColumnaUsuario()
+{
+    int columna = 0;
+    cout << "Ingresa la COLUMNA en la que desea jugar: ";
+    cin >> columna;
+    return columna - 1;
+}
 
-	int Juego::solicitarColumnaUsuario()
-	{
-		int columna = 0;
-		cout << "Ingresa la COLUMNA en la que desea jugar: ";
-		cin >> columna;
-		return columna - 1;
-	}
+bool Juego::jugadorGana()
+{
+    int conteo = this->tablero.contarCeldasSinMinasYSinDescubrir();
+    return conteo == 0;
+}
 
-	bool Juego::jugadorGana()
-	{
-		int conteo = this->tablero.contarCeldasSinMinasYSinDescubrir();
-		if (conteo == 0)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+// Mejora de vidas (Astrid)
+void Juego::iniciar()
+{
+    tiempoInicio = std::chrono::steady_clock::now(); // Iniciar el cronómetro: guarda el tiempo actual al inicio del juego. (Dulce Reyes)
+    std::cout << "El cronometro ha iniciado. ¡Buena suerte!" << std::endl;
+    int fila, columna;
+    while (true)
+    {
+        auto tiempoActual = std::chrono::steady_clock::now(); // Mostrar el tiempo transcurrido en cada iteración del juego (Dulce Reyes)
+        std::chrono::duration<double> duracion = tiempoActual - tiempoInicio;
+        std::cout << "Tiempo transcurrido: " << duracion.count() << " segundos." << std::endl;
 
+        this->tablero.imprimir();
+        cout << "Te quedan: " << this->vidas << " vidas" << endl;
+        cout << "Puntuacion actual: " << this->puntuacion << endl << endl;
 
-//mejora de vidas (Astrid)
-	void Juego::iniciar()
-	{
-		int fila, columna;
-		while (true)
-		{
-			this->tablero.imprimir();
-			cout << "Te quedan: " << this->vidas << " vidas" << endl;
+        fila = this->solicitarFilaUsuario();
+        columna = this->solicitarColumnaUsuario();
+        bool respuestaAUsuario = this->tablero.descubrirMina(columna, fila);
 
-			//Marcador de puntos
-			cout << "Puntuacion actual: " << this->puntuacion << endl << endl;//Puntuacion inicial
-
-			fila = this->solicitarFilaUsuario();
-			columna = this->solicitarColumnaUsuario();
-			bool respuestaAUsuario = this->tablero.descubrirMina(columna, fila);
-
-			if (!respuestaAUsuario) //si descrubre una mina
-			{
-			    this->vidas--; //reducir una vida
-
-			    if (this -> vidas <= 0) //cuando no queden vidas
-                {
-                cout <<" Te has quedado sin vidas! Perdiste el juego" << endl;
-
-			    this->puntuacion -= this->puntosPorMina;//Resta los puntos por mina encontrada
-
-			    cout << "\n Has descubierto una mina!"<< endl << endl;
-			    cout <<  "--Pierdes: " << this->puntosPorMina << " puntos" << endl;//Muestra los puntos perdidos
-
-			    if (this -> vidas <= 0) //cuando no queden vidas
-                {
-                this->perdidas++; //Suma las perdidas
-                cout <<"\n Te has quedado sin vidas! Perdiste el juego" << endl;
-
-                cout << "\nPuntuacion final: " << this->puntuacion << endl;//Muestra la puntuacion final
-
-				this->tablero.setModoDesarrollador(true);
-				this->tablero.imprimir();
-				break;
-                }
-                else //si aun quedan vidas
-                {
+        if (!respuestaAUsuario)
+        {
+            this->vidas--;
+            if (this->vidas <= 0)
+            {
+                cout << "\n Te has quedado sin vidas! Perdiste el juego" << endl;
+                cout << "\nPuntuacion final: " << this->puntuacion << endl;
+                this->tablero.setModoDesarrollador(true);
+                this->tablero.imprimir();
+                break;
+            }
+            else
+            {
                 cout << "--Te quedan " << this->vidas << " vidas\n" << endl;
                 system("pause");
-                continue; //continuar el juego
-                }
-			}
-			else //Si se descubre una mina sin celda
-            {
-                int puntosPorCelda = this->tablero.getMinasCercanas(fila,columna);
-                this->puntuacion += puntosPorCelda; //Aumentar la puntuacion
-                cout << "\n!Ganas " << puntosPorCelda << " puntos!\n\n";
+                continue;
             }
+        }
+        else
+        {
+            int puntosPorCelda = this->tablero.getMinasCercanas(fila, columna);
+            this->puntuacion += puntosPorCelda;
+            cout << "\n!Ganas " << puntosPorCelda << " puntos!\n\n";
+        }
 
-			if (this->jugadorGana())
-			{
-			    this->victorias++; //Suma las victorias
-				cout << " Ganaste el Juego!" << endl;
-				cout << "\Puntuaci n final: " << this->puntuacion << endl<< endl;
-				this->tablero.setModoDesarrollador(true);
-				this->tablero.imprimir();
-				break;
-			}
-			else //si aun quedan vidas
-            {
-                cout << " Has descubierto una mina! Te quedan " << this->vidas << " vidas" << endl;
-                system("pause");
-                continue; //continuar el juego
-            }
-			}
+        if (this->jugadorGana())
+        {
+            cout << " Ganaste el Juego!" << endl;
+            cout << "\nPuntuación final: " << this->puntuacion << endl;
+            this->tablero.setModoDesarrollador(true);
+            this->tablero.imprimir();
+            break;
+        }
+    }
+}
 
-			if (this->jugadorGana())
-			{
-				cout << " Ganaste el Juego!" << endl;
-				this->tablero.setModoDesarrollador(true);
-				this->tablero.imprimir();
-				break;
-			}
-		}
-	}
-
-	//Implementacion de registro de usuario
-	bool Juego::registrarUsuario(const std::string& nombre) {
-    if (nombre.empty()) {
+// Implementacion de registro de usuario
+bool Juego::registrarUsuario(const std::string& nombre)
+{
+    if (nombre.empty())
+    {
         return false;
     }
     usuarioActual = nombre;
     return true;
-    }
+}
 
-    void Juego::setUsuarioActual(const std::string& nombre) {
-        this->usuarioActual = nombre;
-    }
+void Juego::setUsuarioActual(const std::string& nombre)
+{
+    this->usuarioActual = nombre;
+}
 
-    std::string Juego::getUsuarioActual() const {
-        return usuarioActual;
-    }
-//Se obtiene el total de victorias y de perdidas
-int Juego::obtenerVictorias() {
+std::string Juego::getUsuarioActual() const
+{
+    return usuarioActual;
+}
+
+// Se obtiene el total de victorias y de perdidas
+int Juego::obtenerVictorias()
+{
     return this->victorias;
 }
 
-int Juego::obtenerPerdidas() {
+int Juego::obtenerPerdidas()
+{
     return this->perdidas;
 }
 
-
-//implement  el metodo dibujarportada() que estaba en el juego.h pero no se implementaba aqu  (Astrid)
-	void Juego::dibujarPortada (string nombreArchivo){
-	    //se crea un objeto ifstream llamado archivo, y se asocia con el archivo (parametro)
-        ifstream archivo(nombreArchivo); //ifstream es una clase que permite leer datos de un archivo
-        string linea; //almacena cada linea le da del archivo
-
-        //verificar si se abri  el archivo
-    if (archivo.is_open()) {
-
-            //leer el archivo linea por linea hasta llegar al final
-        while (getline(archivo, linea)) { // el getline() extrae una linea del archivo y la almecena en la variable linea
-            cout << linea << endl; //muestra cada l nea leida
+// Implementé el método dibujarPortada() que estaba en el juego.h pero no se implementaba aquí (Astrid)
+void Juego::dibujarPortada(string nombreArchivo)
+{
+    ifstream archivo(nombreArchivo);
+    string linea;
+    if (archivo.is_open())
+    {
+        while (getline(archivo, linea))
+        {
+            cout << linea << endl;
         }
-    //cerrar el archivo despues d leerlo
         archivo.close();
-    } else {
-        cout << "El archivo de portada no se pudo abrir" << endl; //por si no se puede abrir el archivo
+    }
+    else
+    {
+        cout << "El archivo de portada no se pudo abrir" << endl;
         cout << " Bienvenidos al buscaminas!" << endl;
     }
-
-        //el usuario debe presionar una tecla
-        cin.get(); //el cin.get lee un caracter del teclado, tambien enter
-	}
+    cin.get();
+}
